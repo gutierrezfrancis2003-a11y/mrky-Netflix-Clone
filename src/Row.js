c7a6f31155ae18e7d03e297bde5ae2e37e19cb5a
gutierrezfrancis2003-a11y <gutierrezfrancis2003@gmail.com>
2026-03-31 19:26:28 +0800

    save

--- src/Row.js
+++ src/Row.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import axios from './axios';
 
 function Row({ tittle }) {
     const [movies, setMovies] = React.useState([]);
@@ -6,7 +7,13 @@ function Row({ tittle }) {
   // A snippet of code which runs based on a specific condition/variable
   useEffect(() => {
     // if [], run once when the row loads, and don't run again
-  }, []);
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+  
+}, []);
   return (
     <div>
       <h2>{tittle}</h2>
