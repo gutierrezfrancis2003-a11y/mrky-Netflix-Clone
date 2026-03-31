73dc941dc0c4a8ccf7e504e0353200b478865393
gutierrezfrancis2003-a11y <gutierrezfrancis2003@gmail.com>
2026-03-31 17:10:48 +0800

    change

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -6,26 +6,26 @@ import { Counter } from "$/components/counter";
 
 export function App() {
     const readDoccs = new Intl.ListFormat("en", {
-  style: "long",
-  type: "disjunction",
-}).format(["vite", "jsx / tsx", "alpinejs", "kitajs"]);
+      style: "long",
+      type: "disjunction",
+    }).format(["vite", "jsx / tsx", "alpinejs", "kitajs"]);
 
   return (
     <div>
-      <a href="https://vite.dev" target="_blank">
+      <a href="https://www.typescriptlang.org/" target="_blank" rel="noopener noreferrer">
         <img src={viteLogo} class="logo" alt="Vite logo" />
       </a>
-      <a href="https://www.typescriptlang.org/" target="_blank">
+      <a href="https://www.typescriptlang.org/" target="_blank" rel="noopener noreferrer">
         <img src={jsxLogo} class="logo vanilla" alt="TSX logo" />
       </a>
-      <a href="https://alpinejs.dev/start-here" target="_blank">
+      <a href="https://www.typescriptlang.org/" target="_blank" rel="noopener noreferrer">
         <img
           src={alpineLogo}
           class="logo"
           alt="Alpine logo"
         />
       </a>
-      <a href="https://kitajs.org/html/configuration" target="_blank">
+      <a href="https://www.typescriptlang.org/" target="_blank" rel="noopener noreferrer">
         <img src={kitaLogo} class="logo" alt="Kitajs HTML logo" />
       </a>
       <h1>ViTA aka Vite + TSX + Alpine.js</h1>
